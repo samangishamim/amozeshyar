064716c78b0efd7f4e0643c558bf111d5d313189
samangishamim <samangi624@gmail.com>
2024-04-16 16:01:54 +0430

    change the class into interface

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -1,4 +1,13 @@
 package base.service;
 
-public class BaseService {
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+
+public interface BaseService <T extends BaseEntity<ID>, ID extends Serializable>{
+    T saveOrUpdate(T entity);
+
+    T findById(ID id);
+
+    void delete(T t);
 }
