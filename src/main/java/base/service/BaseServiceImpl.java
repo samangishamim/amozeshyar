d0d46f1f47f9433cc5e51a07fbdae41ad296c065
samangishamim <samangi624@gmail.com>
2024-04-16 16:06:53 +0430

    add all the implement methods and fix the methods with annotation

--- src/main/java/base/service/BaseServiceImpl.java
+++ src/main/java/base/service/BaseServiceImpl.java
@@ -1,4 +1,62 @@
 package base.service;
 
-public class BaseServiceImpl {
+import base.entity.BaseEntity;
+import base.exception.NotFoundException;
+import base.repository.BaseRepository;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
+
+@RequiredArgsConstructor
+public class BaseServiceImpl<T extends BaseEntity<ID>,
+        ID extends Serializable,
+        R extends BaseRepository<T, ID>>
+        implements BaseService<T, ID> {
+
+
+    private final R repository;
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public T saveOrUpdate(T entity) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            T t = repository.saveOrUpdate(entity);
+            transaction.commit();
+            return t;
+        } catch (Exception e) {
+            assert transaction != null;
+            transaction.rollback();
+            return null;
+        }
+    }
+
+    @Override
+    public T findById(ID id) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            T foundEntity = repository.findById(id).get();
+            session.getTransaction().commit();
+            return foundEntity;
+        } catch (Exception e) {
+            throw new NotFoundException(String.format("entity with %s not found", id));
+        }
+    }
+
+    @Override
+    public void delete(T t) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            repository.delete(t);
+            transaction.commit();
+        } catch (Exception e) {
+            assert transaction != null;
+            transaction.rollback();
+        }
+    }
 }
