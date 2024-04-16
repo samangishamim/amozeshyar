0c4dec4826471e841aeddb11db027f0beab0c057
samangishamim <samangi624@gmail.com>
2024-04-16 16:11:29 +0430

    create the person class

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,19 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "person")
+public class Person {
+
+
+}
