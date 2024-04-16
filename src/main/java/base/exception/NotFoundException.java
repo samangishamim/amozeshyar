d054cc5bd16e25df93d6b16d3f1b7f3ecbd1f6f9
samangishamim <samangi624@gmail.com>
2024-04-16 15:36:06 +0430

    add a method for get the message

--- src/main/java/base/exception/NotFoundException.java
+++ src/main/java/base/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package base.exception;
 
-public class NotFoundException {
+public class NotFoundException extends  RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+
 }
