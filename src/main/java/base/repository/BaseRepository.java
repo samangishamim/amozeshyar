27dccff9576df32778678f7d5953e7919526b353
samangishamim <samangi624@gmail.com>
2024-04-16 15:42:29 +0430

    add generic types

---
+++ src/main/java/base/repository/BaseRepository.java
@@ -0,0 +1,15 @@
+package base.repository;
+
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+public interface BaseRepository <T extends BaseEntity<ID>, ID extends Serializable> {
+
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void delete(T entity);
+}
