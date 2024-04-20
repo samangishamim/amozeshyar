baba5adf79312d6b502d261b5334a3254553453b
samangishamim <samangi624@gmail.com>
2024-04-20 14:58:08 +0430

    create the student class

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+
+@Entity
+@DiscriminatorValue(value = "Student")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Student extends Person {
+    private Double gpa;
+    private Integer unitsTaken;
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "STUDENT_COURSE",
+            joinColumns = { @JoinColumn(name = "STUDENT_ID") },
+            inverseJoinColumns = { @JoinColumn(name = "COURSE_ID") }
+    )
+    private Set<Course> courses = new HashSet<>();
+}
