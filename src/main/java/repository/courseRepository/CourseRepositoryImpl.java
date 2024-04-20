fbd115022d90218448f0ee4bb88f6a6501739a80
samangishamim <samangi624@gmail.com>
2024-04-20 17:39:10 +0430

    fix bugs

--- src/main/java/repository/courseRepository/CourseRepositoryImpl.java
+++ src/main/java/repository/courseRepository/CourseRepositoryImpl.java
@@ -2,9 +2,12 @@ package repository.courseRepository;
 
 import base.repository.BaseRepositoryImpl;
 import model.Person;
+import org.hibernate.SessionFactory;
 import repository.personRepository.PersonRepository;
 
 public class CourseRepositoryImpl extends BaseRepositoryImpl<Person,Long> implements PersonRepository {
+
+
 
     @Override
     public Class<Person> getEntityClass() {
