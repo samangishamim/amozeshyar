f73d757de2615f5ffed16e9515e751e82eca137b
samangishamim <samangi624@gmail.com>
2024-04-20 14:58:23 +0430

    create the Professor class

---
+++ src/main/java/model/Professor.java
@@ -0,0 +1,26 @@
+package model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+
+@Entity
+@DiscriminatorValue(value = "Professor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Professor extends Person {
+    private Double salary;
+
+    @OneToMany(mappedBy = "professor", cascade = { CascadeType.ALL })
+    private Set<Course> coursesTaught = new HashSet<>();
+}
