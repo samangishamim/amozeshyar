5a76ff1e033b0736092a70820a6f095c435dd7f8
samangishamim <samangi624@gmail.com>
2024-04-20 17:40:34 +0430

    correct the writing

--- src/main/java/repository/professorRepository/ProfessorRepositoryImpl.java
+++ src/main/java/repository/professorRepository/ProfessorRepositoryImpl.java
@@ -2,9 +2,12 @@ package repository.professorRepository;
 
 import base.repository.BaseRepositoryImpl;
 import model.Person;
+import org.hibernate.SessionFactory;
 import repository.personRepository.PersonRepository;
 
 public class ProfessorRepositoryImpl extends BaseRepositoryImpl<Person,Long> implements PersonRepository {
+
+
     @Override
     public Class<Person> getEntityClass() {
         return null;
