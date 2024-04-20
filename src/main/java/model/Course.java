25983ebbbd14f3772d7b6e73267f3792cb847e66
samangishamim <samangi624@gmail.com>
2024-04-20 15:02:24 +0430

    create course class

---
+++ src/main/java/model/Course.java
@@ -0,0 +1,27 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer units;
+    private Integer grade;
+
+    @ManyToOne
+    @JoinColumn(name = "PROFESSOR_ID")
+    private Professor professor;
+
+}
