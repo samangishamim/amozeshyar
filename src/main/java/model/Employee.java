032b75f904316d846d42872b7df4242f9ac02c6e
samangishamim <samangi624@gmail.com>
2024-04-20 15:00:04 +0430

    create the Employee class

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,21 @@
+package model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue(value = "Employee")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee extends Person {
+    private Double salary;
+    private String role;
+
+}
