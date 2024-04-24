543ca37a1fecd04beed7684a9697ee62069b00d5
shamim <shamimsamangi5@gmail.com>
2024-04-24 22:54:30 +0330

    add new method findall

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -3,6 +3,7 @@ package base.repository;
 import base.entity.BaseEntity;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Optional;
 
 public interface BaseRepository <T extends BaseEntity<ID>, ID extends Serializable> {
@@ -12,4 +13,6 @@ public interface BaseRepository <T extends BaseEntity<ID>, ID extends Serializab
     Optional<T> findById(ID id);
 
     void delete(T entity);
+
+    List<T> findAll();
 }
