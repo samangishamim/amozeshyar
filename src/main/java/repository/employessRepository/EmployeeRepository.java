cba9f95f5e89c82c2786223222bff6a246490b1c
shamim <shamimsamangi5@gmail.com>
2024-04-25 16:28:34 +0330

    add new method for paying and findByNationalId

--- src/main/java/repository/employessRepository/EmployeeRepository.java
+++ src/main/java/repository/employessRepository/EmployeeRepository.java
@@ -5,6 +5,11 @@ import model.Employee;
 import model.Person;
 import repository.personRepository.PersonRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends BaseRepository<Employee, Long> {
+    EmployeeRepositoryImpl.EmployeePay pay(Long id);
+    Optional<Employee> findByNationalId(String nationalId);
+    boolean checkNationalIdAndPassword(String nationalId, String password);
 
 }
