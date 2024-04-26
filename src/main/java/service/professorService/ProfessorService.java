bb457f2ce8bf76a0bffe72c441d6979990773001
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:26:27 +0330

    add new methods and implement all of it and complete the remaining code

--- src/main/java/service/professorService/ProfessorService.java
+++ src/main/java/service/professorService/ProfessorService.java
@@ -3,5 +3,10 @@ package service.professorService;
 import base.service.BaseService;
 import model.Professor;
 
+import java.util.Optional;
+
 public interface ProfessorService extends BaseService<Professor, Long> {
+    public Optional<Professor> professorSignIn(String nationalId, String password);
+    public Optional<Professor> professorInfo(String nationalId);
+    boolean checkNationalIdAndPassword(String nationalId, String password);
 }
--- src/main/java/service/professorService/ProfessorServiceImpl.java
+++ src/main/java/service/professorService/ProfessorServiceImpl.java
@@ -1,12 +1,64 @@
 package service.professorService;
 
 import base.service.BaseServiceImpl;
+import connection.SessionFactorySingleton;
+import exception.NotFoundException;
+import model.Person;
 import model.Professor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import repository.professorRepository.ProfessorRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ProfessorServiceImpl extends BaseServiceImpl<Professor, Long, ProfessorRepository> implements ProfessorService{
     public ProfessorServiceImpl(ProfessorRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
+    @Override
+    public Optional<Professor> professorSignIn(String nationalId, String password) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<Professor> find = repository.professorSignIn(nationalId, password);
+            find.orElseThrow(() -> new NotFoundException("Entity not found"));
+            session.getTransaction().commit();
+            return find;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Professor> professorInfo(String nationalId) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<Professor> find = repository.professorInfo(nationalId);
+            find.orElseThrow(() -> new NotFoundException("Entity not found"));
+            session.getTransaction().commit();
+            return find;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean checkNationalIdAndPassword(String nationalId, String password) {
+        SessionFactory factory = SessionFactorySingleton.getInstance();
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+
+            Query<Person> query = session.createQuery("FROM Professor p " +
+                    "WHERE p.nationalId = :nId AND p.password = :pass");
+            query.setParameter("nId", nationalId);
+            query.setParameter("pass", password);
+            List<Person> resultList = query.getResultList();
+
+            session.getTransaction().commit();
+            return resultList.isEmpty();
+        }
     }
 }
