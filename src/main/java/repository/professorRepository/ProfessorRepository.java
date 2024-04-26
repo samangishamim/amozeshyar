df47f8eab14906122fe6c29709ece8ad8e8e2e5d
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:26:04 +0330

    add new methods and implement all of it and complete the remaining code

--- src/main/java/repository/professorRepository/ProfessorRepository.java
+++ src/main/java/repository/professorRepository/ProfessorRepository.java
@@ -4,5 +4,9 @@ import base.repository.BaseRepository;
 import model.Person;
 import model.Professor;
 
+import java.util.Optional;
+
 public interface ProfessorRepository extends BaseRepository<Professor, Long> {
+    public Optional<Professor> professorSignIn(String nationalId , String password);
+    public Optional<Professor> professorInfo(String nationalId);
 }
--- src/main/java/repository/professorRepository/ProfessorRepositoryImpl.java
+++ src/main/java/repository/professorRepository/ProfessorRepositoryImpl.java
@@ -3,8 +3,12 @@ package repository.professorRepository;
 import base.repository.BaseRepositoryImpl;
 import model.Person;
 import model.Professor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import repository.personRepository.PersonRepository;
+
+import java.util.Optional;
 
 public class ProfessorRepositoryImpl extends BaseRepositoryImpl<Professor,Long> implements ProfessorRepository {
 
@@ -15,6 +19,37 @@ public class ProfessorRepositoryImpl extends BaseRepositoryImpl<Professor,Long> 
 
     @Override
     public Class<Professor> getEntityClass() {
-        return null;
+        return Professor.class;
     }
+
+    @Override
+    public String getMyClass() {
+        return "Professor";
+    }
+
+
+    @Override
+    public Optional<Professor> professorSignIn(String nationalId, String password) {
+        Session session = sessionFactory.getCurrentSession();
+        String hql ="FROM %s WHERE professorId=:professorId AND password=:password ";
+        Query<Professor> query = session.createQuery(String.format(hql , getMyClass()),getEntityClass());
+        query.setParameter("professorId", nationalId );
+        query.setParameter( "password" , password );
+        Object result = query.uniqueResult();
+
+        return Optional.ofNullable(query.getSingleResult());
+    }
+
+    @Override
+    public Optional<Professor> professorInfo(String nationalId) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Professor> query = session.createQuery("FROM Professor p  " +
+                " WHERE p.professorId=:professorId" , Professor.class);
+        query.setParameter("professorId", nationalId );
+        Professor professor = query.uniqueResult();
+
+        return Optional.ofNullable(query.getSingleResult());
+    }
+
+
 }
