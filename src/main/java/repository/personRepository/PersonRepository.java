2443a9765ec5e91d1560a6f256a6e54cb9c7c519
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:24:40 +0330

    add new methods and implement all of it

--- src/main/java/repository/personRepository/PersonRepository.java
+++ src/main/java/repository/personRepository/PersonRepository.java
@@ -4,4 +4,5 @@ import base.repository.BaseRepository;
 import model.Person;
 
 public interface PersonRepository extends BaseRepository <Person,Long>{
+    boolean signIn(String username, String password);
 }
--- src/main/java/repository/personRepository/PersonRepositoryImpl.java
+++ src/main/java/repository/personRepository/PersonRepositoryImpl.java
@@ -16,6 +16,24 @@ public class PersonRepositoryImpl extends BaseRepositoryImpl<Person,Long> implem
 
     @Override
     public Class getEntityClass() {
+        return Person.class;
+    }
+
+    @Override
+    public String getMyClass() {
         return null;
+    }
+
+    @Override
+    public boolean signIn(String username, String password) {
+        SessionFactory factory = SessionFactorySingleton.getInstance();
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            Person person = session.get(Person.class, 1L);
+            if (person.getUsername().equals(username) && person.getPassword().equals(password)) {
+                return true;
+            }
+            return false;
+        }
     }
 }
