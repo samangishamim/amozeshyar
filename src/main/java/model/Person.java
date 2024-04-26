bed4544d67549ee92c312107b789581f43ca908d
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:24:11 +0330

    delete most of the lombok annotations and do it myself

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -7,23 +7,53 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
-@Entity
+
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "person_type", discriminatorType = DiscriminatorType.STRING)
+
+@Table(name = "persons")
+
+@Entity
 public class Person extends BaseEntity<Long>{
 
 
-
+    @Column(name = "first_name" , nullable = false)
     private String firstName;
 
+    @Column(name = "last_name" , nullable = false)
     private String lastName;
 
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
+    public Person(Long aLong, String firstName, String lastName, String username, String password) {
+        super(aLong);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Person(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
