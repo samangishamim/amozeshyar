5dca0817b696df0362c0df1e31501a63fd9f8849
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:28:35 +0330

    create a new class  grade for each semester

---
+++ src/main/java/model/SemesterGrade.java
@@ -0,0 +1,40 @@
+package model;
+
+import base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity(name = "semester_grade")
+public class SemesterGrade extends BaseEntity<Long> {
+
+    @OneToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @Column(nullable = false)
+    private Double semester;
+
+    @Column(name = "final_score", nullable = false)
+    private Double finalScore;
+
+    @Override
+    public String toString() {
+        return "SemesterGrade{" +
+                "student=" + student +
+                ", semester=" + semester +
+                ", finalScore=" + finalScore +
+                ", id=" + id +
+                '}';
+    }
+}
