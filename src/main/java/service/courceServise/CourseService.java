154865c57b1e0d08f08fa95c12c1db05681e2d2f
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:20:29 +0330

    add new methods and implement all of it

--- src/main/java/service/courceServise/CourseService.java
+++ src/main/java/service/courceServise/CourseService.java
@@ -2,9 +2,14 @@ package service.courceServise;
 
 import base.service.BaseService;
 import model.Course;
+import model.Professor;
+
+import java.util.Optional;
 
 public interface CourseService extends BaseService<Course, Long> {
 
     void addCourse(Course course,Long professorId);
+    public Optional<Long> professorSemesterSalary(Professor professor , int semester);
+    boolean findCourseByIdOfCourse(int year, int semester, int idOfCourse);
 
 }
--- src/main/java/service/courceServise/CourseServiceImpl.java
+++ src/main/java/service/courceServise/CourseServiceImpl.java
@@ -1,30 +1,22 @@
 package service.courceServise;
 
 import base.service.BaseServiceImpl;
+import exception.NotFoundException;
 import model.Course;
+import model.Professor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import repository.courseRepository.CourseRepository;
+
+import java.util.Optional;
 
 public class CourseServiceImpl extends BaseServiceImpl<Course,Long,CourseRepository>implements CourseService{
     public CourseServiceImpl(CourseRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
     }
 
-    @Override
-    public Course saveOrUpdate(Course entity) {
-        return null;
-    }
 
-    @Override
-    public Course findById(Long aLong) {
-        return null;
-    }
 
-    @Override
-    public void delete(Course course) {
-
-    }
 
     @Override
     public void addCourse(Course course, Long professorId) {
@@ -36,5 +28,23 @@ public class CourseServiceImpl extends BaseServiceImpl<Course,Long,CourseReposit
         {
 
         }
+    }
+
+    @Override
+    public Optional<Long> professorSemesterSalary(Professor professor, int semester) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<Long> find = repository.professorSemesterSalary(professor,semester);
+            find.orElseThrow(() -> new NotFoundException("Entity not found"));
+            session.getTransaction().commit();
+            return find;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean findCourseByIdOfCourse(int year, int semester, int idOfCourse) {
+        return repository.findCourseByIdOfCourse(year, semester, idOfCourse);
     }
 }
