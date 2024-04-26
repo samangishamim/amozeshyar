53c26c92f61f1f815ddd42cb31e8580bcca0c702
shamim <shamimsamangi5@gmail.com>
2024-04-26 15:25:19 +0330

    delete most of the lombok annotations

--- src/main/java/model/RegisterCourse.java
+++ src/main/java/model/RegisterCourse.java
@@ -4,14 +4,16 @@ import base.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
+
+@Table(name = "register_courses")
 
 public class RegisterCourse  extends BaseEntity<Long> {
 
@@ -19,6 +21,25 @@ public class RegisterCourse  extends BaseEntity<Long> {
     private boolean isPassed;
     private Long studentId;
     private Long courseId;
+
+    public RegisterCourse(Long aLong, double mark, boolean isPassed, Long studentId, Long courseId, Student student, Course course) {
+        super(aLong);
+        this.mark = mark;
+        this.isPassed = isPassed;
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.student = student;
+        this.course = course;
+    }
+
+    public RegisterCourse(double mark, boolean isPassed, Long studentId, Long courseId, Student student, Course course) {
+        this.mark = mark;
+        this.isPassed = isPassed;
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.student = student;
+        this.course = course;
+    }
 
     @ManyToOne
     @JoinColumn(name="student_id",insertable = false,updatable = false)
