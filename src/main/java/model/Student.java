2978a1afc1213d47b059b49a886452074f718c0f
shamim <shamimsamangi5@gmail.com>
2024-05-02 00:49:36 +0330

    add new constructor and toString

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -22,22 +22,28 @@ public class Student extends Person {
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<RegisterCourse> registerCourses;
 
+    public Student() {
+    }
 
-    public Student(String firstName, String lastName, String username, String password, List<RegisterCourse> registerCourses) {
-        super(firstName, lastName, username, password);
+    public Student(Long aLong, String firstName, String lastName, String nationalid, String password, List<RegisterCourse> registerCourses) {
+        super(aLong, firstName, lastName, nationalid, password);
         this.registerCourses = registerCourses;
     }
 
-    public Student(Long aLong, String firstName, String lastName, String username, String password, List<RegisterCourse> registerCourses) {
-        super(aLong, firstName, lastName, username, password);
+    public Student(String firstName, String lastName, String nationalid, String password, List<RegisterCourse> registerCourses) {
+        super(firstName, lastName, nationalid, password);
         this.registerCourses = registerCourses;
+    }
+    public Student(String firstName, String lastName, String nationalid, String password) {
+        super(firstName, lastName, nationalid, password);
+    }
+
+    public Student(Long aLong, String firstName, String lastName, String nationalid, String password) {
+        super(aLong, firstName, lastName, nationalid, password);
     }
 
     @Override
     public String toString() {
-        return "Student{" +
-                "registerCourses=" + registerCourses +
-                ", id=" + id +
-                '}';
+        return super.toString();
     }
 }
