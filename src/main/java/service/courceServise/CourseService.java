c72e21ed06515f7a5a40b8833eedd964be49b781
shamim <shamimsamangi5@gmail.com>
2024-05-02 15:08:54 +0330

    add new methods for findBySemester

--- src/main/java/service/courceServise/CourseService.java
+++ src/main/java/service/courceServise/CourseService.java
@@ -4,12 +4,14 @@ import base.service.BaseService;
 import model.Course;
 import model.Professor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseService extends BaseService<Course, Long> {
 
     void addCourse(Course course,Long professorId);
     public Optional<Long> professorSemesterSalary(Professor professor , int semester);
-    public Optional<Course> findByCourseCode(Integer courseCode);
+    public List<Course> findBySemester(Integer semester);
+    List<Course> findCoursesByYearAndSemesterAndProfessorId(int year, int semester, Long professorId);
 
 }
