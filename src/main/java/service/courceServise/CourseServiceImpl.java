b48b9df4883692805cc5edb768182e35b187f61d
shamim <shamimsamangi5@gmail.com>
2024-05-02 15:09:22 +0330

    correct the error

--- src/main/java/service/courceServise/CourseServiceImpl.java
+++ src/main/java/service/courceServise/CourseServiceImpl.java
@@ -8,6 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import repository.courseRepository.CourseRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class CourseServiceImpl extends BaseServiceImpl<Course,Long,CourseRepository>implements CourseService{
@@ -44,15 +45,33 @@ public class CourseServiceImpl extends BaseServiceImpl<Course,Long,CourseReposit
     }
 
     @Override
-    public Optional<Course> findByCourseCode(Integer courseCode) {
+    public List<Course> findBySemester(Integer semester) {
         try (Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
-            Optional<Course> find = repository.findByCourseCode(courseCode);
+            Optional<List<Course>> find = repository.findBySemester(semester);
             find.orElseThrow(() -> new NotFoundException("Entity not found"));
             session.getTransaction().commit();
-            return find;
+            return find.get();
         } catch (Exception e) {
-            return Optional.empty();
+            return null;
+        }
+    }
+
+    @Override
+    public List<Course> findCoursesByYearAndSemesterAndProfessorId(int year, int semester, Long professorId) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<List<Course>> optionalCourseList =
+                    repository.findCourseByYearAndSemesterAndProfessorId(year, semester, professorId);
+            optionalCourseList.orElseThrow(
+                    () -> new NotFoundException(
+                            String.format("Course/courses with year: %s semester: %s for professor id: %s not found",
+                                    year, semester, professorId))
+            );
+            return optionalCourseList.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
